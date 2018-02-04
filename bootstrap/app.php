26a0ef5826b046b105a0b3b99c0a77a70b1b2a7f
xx19941215 <xiaoxiao94@outlook.com>
2018-02-04 14:40:45 +0800

    add:.env.example

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,7 +4,6 @@ $basePath = realpath(__DIR__ . '/../');
 require $basePath . '/vendor/autoload.php';
 
 
-
 try {
     (new Dotenv\Dotenv(__DIR__.'/../'))->load();
 } catch (Dotenv\Exception\InvalidPathException $e) {
--- public/server.php
+++ public/server.php
@@ -10,16 +10,6 @@ $http = new swoole_http_server('127.0.0.1', '8888');
 $http->set($swooleConfig);
 
 $http->on('request', function ($swRequest, $swResponse) use ($app) {
-    // 每次请求都只会执行这里面的代码，不用再初始化框架内核，运行性能大大提高！
-//    $static = __DIR__.$swRequest->server['path_info'];
-//    if (file_exists($static)) {
-//        $ext = pathinfo($static, PATHINFO_EXTENSION);
-//        $swResponse->header('Content-Type', sprintf('text/%s', $ext));
-//        $swResponse->end(file_get_contents($static));
-//
-//        return;
-//    }
-
     $request = toSfRequest($swRequest);
     $response = $app->handle($request);
     foreach ($response->headers->allPreserveCase() as $key => $vals) {
